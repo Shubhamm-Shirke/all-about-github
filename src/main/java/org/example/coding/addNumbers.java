1a57eac3c7c9852072552fb6502b5a042e395a86
Shubham <shirkeshubham986@gmail.com>
2025-10-13 20:57:40 +0530

    2nd commit

--- src/main/java/org/example/coding/addNumbers.java
+++ src/main/java/org/example/coding/addNumbers.java
@@ -2,6 +2,7 @@ package org.example.coding;
 
 public class addNumbers {
     public static void main(String[] args) {
-        System.out.println(1+2);
+        System.out.println(2+2);
+        System.out.println("one +"+" one = "+" two");
     }
 }
