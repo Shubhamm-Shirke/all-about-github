378221c8bc339edf859776b4e935d0aa6e5544b4
Shubham <shirkeshubham986@gmail.com>
2025-10-15 21:45:14 +0530

    1st work

--- src/main/java/org/example/code/Code2.java
+++ src/main/java/org/example/code/Code2.java
@@ -1,7 +1,7 @@
 package org.example.code;
 
-public class Code {
+public class Code2 {
     public static void main(String[] args) {
-        System.out.println("Code1");
+        System.out.println("code2");
     }
 }
